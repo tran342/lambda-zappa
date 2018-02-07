c4164a856b6b850e9ef29fb3ee1f91c5c3ba10c5
Khoa Tran <tran342@gmail.com>
2018-02-08 00:11:31 +0700

    Change region

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 cd $TRAVIS_BUILD_DIR/src/backend
-zappa update dev
 python manage.py collectstatic --noinput --settings=conf.staging
--- src/backend/conf/staging.py
+++ src/backend/conf/staging.py
@@ -5,6 +5,7 @@ ALLOWED_HOSTS = ['localhost', '.execute-api.ap-southeast-1.amazonaws.com']
 AWS_QUERYSTRING_AUTH = False
 AWS_ACCESS_KEY_ID = 'AKIAIAXGGQ5APIHMBKFA'
 AWS_SECRET_ACCESS_KEY = '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'
+AWS_S3_HOST = 's3-ap-southeast-1.amazonaws.com'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 AWS_STORAGE_BUCKET_NAME = 'zappa-lambda-assets'
