9dcf9603c54d5c96aeb63e037d594a1323653574
Khoa Tran <tran342@gmail.com>
2018-02-07 23:36:56 +0700

    Change region

--- src/backend/conf/staging.py
+++ src/backend/conf/staging.py
@@ -1,11 +1,25 @@
 from base import *  # noqa
 
-ALLOWED_HOSTS = ['localhost', '.execute-api.us-east-1.amazonaws.com']
+ALLOWED_HOSTS = ['localhost', '.execute-api.ap-southeast-1.amazonaws.com']
 
 AWS_QUERYSTRING_AUTH = False
 AWS_ACCESS_KEY_ID = 'AKIAIAXGGQ5APIHMBKFA'
 AWS_SECRET_ACCESS_KEY = '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'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-AWS_STORAGE_BUCKET_NAME = 'zappa-lambda-static'
+AWS_STORAGE_BUCKET_NAME = 'zappa-lambda-assets'
 AWS_LOCATION = 'admin-staging'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'lambdazappa',
+        'USER': 'root',
+        'PASSWORD': 'qazqaz123',
+        'HOST': 'lambda-zappa.csp92n8eqf6a.ap-southeast-1.rds.amazonaws.com',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"',
+        }
+    }
+}
